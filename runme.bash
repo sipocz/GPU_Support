29ecb96ee07d5559a04b86c04be8422cfda37229
László Sipőcz <sipoczlaszlo@gmail.com>
2021-11-15 00:07:47 +0100

    Update runme.bash

--- runme.bash
+++ runme.bash
@@ -1,3 +1,13 @@
+----------------------------
+LAMBDA_REPO=$(mktemp) && \
+wget -O${LAMBDA_REPO} https://lambdalabs.com/static/misc/lambda-stack-repo.deb && \
+sudo dpkg -i ${LAMBDA_REPO} && rm -f ${LAMBDA_REPO} && \
+sudo apt-get update && sudo apt-get install -y lambda-stack-cuda
+sudo reboot
+---------------------------
+
+
+
 1. tensorflow 2.4.0
 2. CUDNN 8.0
 3. CUDA 11.0
