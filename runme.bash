0621b1fa1f94dcc59fe4cef97e578e06ec3bcd71
László Sipőcz <sipoczlaszlo@gmail.com>
2021-11-14 01:25:14 +0100

    Update runme.bash

--- runme.bash
+++ runme.bash
@@ -1,1 +1,12 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt install nvidia-driver-440
 
+-- reboot
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
+sudo apt update
+sudo apt install cuda
